1835c4263aa99482d80e4ec96d1c7dc28bc2cf7a
abhijna <abhijnabangera2003@gmail.com>
2023-11-20 13:14:57 +0530

    second commit

--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,6 +1,22 @@
 import "../css/Contact.css"
 import react from "react";
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 function Contact() {
+ const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_kvpm8z6', 'template_3m5dg17', form.current, 'i1WyGZbXhGH6i-U7c')
+      .then((result) => {
+          console.log(result.text);
+          alert('email sent successfully');
+          form.current.reset();
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
   return (
     <>
       <div class="Contact">
@@ -9,15 +25,30 @@ function Contact() {
         </h1>
       </div>
         
-      <div class="info">
-        
-        <h3>Abhijna</h3>
-        <h3>4CB21CS001</h3>
-        <h3>Canara Engineering College</h3>
-        
+      <div className="form">
+        <form  ref={form} onSubmit={sendEmail}>
+            <table align="center">
+            <tr>
+                <td><input type="text" name="name" placeholder="Enter your Name"/></td>
+                </tr>
+                <br></br>
+                <tr>
+                <td><input type="text" name="email" placeholder="Enter your email"/></td>
+                </tr>
+                <br></br>
+                <tr>
+                <td><textarea name="message" row="5" cols="25" placeholder="Enter your message"/></td>
+                </tr>
+                <br></br>
+                <div className="but">
+                <tr>
+                    <button>Send Message</button>
+                </tr>
+                </div>
+                </table>
 
-        <div class="information"></div>
-      </div>
+       </form>
+</div>
 
        
       
